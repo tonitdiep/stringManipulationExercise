580ffbb6de55bba2ae0baa425fdd252c79591539
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-03-23 19:00:20 +0000

    question

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+Leetcode: find common characters
+
+Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+*/
